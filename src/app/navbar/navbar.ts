0e453b7403b317e09501ff91347179d3f25d840b
Paterne <diedhioupaterne279@gmail.com>
2026-04-30 15:40:33 +0000

    feat:dark-mode fixed

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,8 +1,8 @@
-import { Component, inject, signal } from '@angular/core'
-import { NgOptimizedImage } from "@angular/common";
-import { Button } from "primeng/button";
-import { Router, RouterLink } from "@angular/router";
-import { AuthService } from '../auth/auth-service';
+import { Component, effect, inject, signal } from '@angular/core'
+import { NgOptimizedImage } from '@angular/common'
+import { Button } from 'primeng/button'
+import { Router, RouterLink } from '@angular/router'
+import { AuthService } from '../auth/auth-service'
 
 @Component({
   selector: 'app-navbar',
@@ -11,22 +11,25 @@ import { AuthService } from '../auth/auth-service';
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-
   // Injection du service d'authentification pour pouvoir afficher le nom de l'utilisateur connecté et gérer la déconnexion
   authService = inject(AuthService)
 
-  isDark = signal(false);
+  readonly DARK_MODE_KEY = 'dark-mode'
+
+  isDark = signal(localStorage.getItem(this.DARK_MODE_KEY) === 'true')
+
+  //creon un fonction qui permet d'exuter du code quand un signal change de valeur
+  constructor() {
+    effect(() => {
+      document.documentElement.classList.toggle('app-dark', this.isDark())
+      localStorage.setItem(this.DARK_MODE_KEY, String(this.isDark()))
+    })
+  }
 
   //injection de la route pour naviguer la deconnexion
   router = inject(Router)
 
-  toggleDarkMode() {
-    this.isDark.update(value => !value);
-    document.documentElement.classList.toggle('app-dark', this.isDark());
-  }
-
-
-  logout = ()=> {
+  logout = () => {
     this.authService.logout().subscribe(() => {
       // La déconnexion a réussi, vous pouvez effectuer des actions supplémentaires ici si nécessaire
       this.router.navigate(['/'])
